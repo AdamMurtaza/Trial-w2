be8f85024e89cbff47a0db0c5eb572b3b08fb79f
AdamMurtaza <adam.murtaza5253@gmail.com>
2026-01-19 14:57:44 +0500

    Update game.js

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 450,
+  backgroundColor: '#87CEEB',
   physics: {
     default: 'arcade',
     arcade: {
@@ -10,7 +11,6 @@ const config = {
     }
   },
   scene: {
-    preload,
     create,
     update
   }
@@ -21,83 +21,43 @@ const game = new Phaser.Game(config);
 let player;
 let platforms;
 let cursors;
-let jumpSound;
-
-function preload () {
-  this.load.image('ground', 'assets/ground.png');
-
-  // Sprite sheet: frameWidth & frameHeight MUST match your sprite
-  this.load.spritesheet('player',
-    'assets/player.png',
-    { frameWidth: 32, frameHeight: 48 }
-  );
-
-  this.load.audio('jump', 'assets/jump.wav');
-}
 
 function create () {
-  // Platforms
-  platforms = this.physics.add.staticGroup();
-  platforms.create(400, 430, 'ground').setScale(2).refreshBody();
-  platforms.create(600, 300, 'ground');
-  platforms.create(50, 250, 'ground');
-  platforms.create(750, 200, 'ground');
-
-  // Player
-  player = this.physics.add.sprite(100, 300, 'player');
-  player.setBounce(0.1);
-  player.setCollideWorldBounds(true);
-
-  // Collision
-  this.physics.add.collider(player, platforms);
-
-  // Controls
   cursors = this.input.keyboard.createCursorKeys();
 
-  // Sound
-  jumpSound = this.sound.add('jump');
+  // Platforms (rectangles)
+  platforms = this.physics.add.staticGroup();
 
-  // Animations
-  this.anims.create({
-    key: 'idle',
-    frames: [{ key: 'player', frame: 0 }],
-    frameRate: 10
-  });
+  platforms.create(400, 430)
+    .setDisplaySize(800, 40)
+    .refreshBody();
 
-  this.anims.create({
-    key: 'run',
-    frames: this.anims.generateFrameNumbers('player', { start: 1, end: 4 }),
-    frameRate: 10,
-    repeat: -1
-  });
+  platforms.create(600, 300)
+    .setDisplaySize(200, 20)
+    .refreshBody();
 
-  this.anims.create({
-    key: 'jump',
-    frames: [{ key: 'player', frame: 5 }],
-    frameRate: 10
-  });
+  platforms.create(200, 220)
+    .setDisplaySize(200, 20)
+    .refreshBody();
+
+  // Player (rectangle)
+  player = this.physics.add.rectangle(100, 300, 40, 50, 0xff0000);
+  player.body.setCollideWorldBounds(true);
+  player.body.setBounce(0.1);
+
+  this.physics.add.collider(player, platforms);
 }
 
 function update () {
   if (cursors.left.isDown) {
-    player.setVelocityX(-200);
-    player.flipX = true;
-    player.anims.play('run', true);
-
+    player.body.setVelocityX(-200);
   } else if (cursors.right.isDown) {
-    player.setVelocityX(200);
-    player.flipX = false;
-    player.anims.play('run', true);
-
+    player.body.setVelocityX(200);
   } else {
-    player.setVelocityX(0);
-    player.anims.play('idle', true);
+    player.body.setVelocityX(0);
   }
 
-  // Jump
   if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-450);
-    jumpSound.play();
-    player.anims.play('jump', true);
+    player.body.setVelocityY(-450);
   }
 }
